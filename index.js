7d5e56a65132e6f056825c63ff9ffa824b11240b
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-05-16 20:05:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-function button() {
-    var love= Math.random() *100;
-love = Math.floor(love)+1;
-    console.log(love);
-
-    if( love >= 50){
-        alert("your love score is "+love+"%"+ " you are are so good together.");
-    }  else{ alert("your love score is "+love+"%"+ " sorry to say, you don't seem like a perfect match.");}
-    }
-
-
-    
-
-    
-  
- 
-    
- 
+function love(){
+    var res= Math.random()*100;
+    res=Math.floor(res)+1;
+    console.log("love")
+    if( res >= 50){
+        alert("your love score is "+res+"%"+ " you are are so good together.");
+    }  else{ alert("your love score is "+res+"%"+ " sorry to say, you don't seem like a perfect match.");}
+}
