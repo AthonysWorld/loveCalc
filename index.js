4dec88b06db24fe8bdd2084f1113a805e98b4218
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-05-16 20:32:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function love(){
     res=Math.floor(res)+1;
     console.log("love")
     if( res >= 50){
-        alert("your love score is "+res+"%"+ " you are are so good together.");
+        alert("your love score is "+res+"%,"+ " Your relationship is perfect and you and your partner are incredibly compatible.");
     }  else{ alert("your love score is "+res+"%"+ " sorry to say, you don't seem like a perfect match.");}
 }
