35730e42e7aaee0ccdb6c718c97e4de9f81850b1
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-05-18 23:06:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function love(){
-    var input = document.getElementById("input1").value
-    var inputTwo = document.getElementById("input2").value
+    var input = document.getElementById("input").value
+    var input2 = document.getElementById("ann").value
     var res= Math.random()*100;
     res=Math.floor(res)+1;
     console.log("love")
     if( res >= 50){
-        alert( input+" your love compatibility score is " + res+ "%," + " Your relationship is perfect, you and "+inputTwo+ " are incredibly compatible.");
-    }  else{ alert(input+" your love score is "+res+"%"+ " sorry to say, you and "+inputTwo+" don't seem like a perfect match.");}
+       document.querySelector("h2").innerHTML=( input +" your love compatibility score is " + res+ "%," + " Your relationship is perfect, you and "+input2+ " are incredibly compatible  &#128525")
+    }  else{ 
+        document.querySelector("h2").innerHTML=input+" your love score is "+res+"%"+ " sorry to say, you and "+input2+" don't seem like a perfect match &#128532"
+    }
 }
